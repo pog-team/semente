26e76df6b82f7da65d77762a3feb78926d6c9707
David Elias <jogadorbr602@gmail.com>
2020-07-02 20:49:07 -0300

    "Adicionado Routas com React Navigation"

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const AppStack = createStackNavigator()
+
+import Welcome from './pages/Welcome'
+import Home from './pages/Home'
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={ {headerShown: false} }>
+                <AppStack.Screen name="Welcome" component={Welcome}></AppStack.Screen>
+                {/* <AppStack.Screen name="Welcome" component={Home}></AppStack.Screen> */}
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
