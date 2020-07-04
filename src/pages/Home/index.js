8fecdf61894be652d96071b0e31fef3c439e9e80
Rodolfobrian <rodolfo_bdf@hotmail.com>
2020-07-04 23:28:20 +0100

    styling the flatList

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,138 +1,138 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Dimensions,} from 'react-native';
 import logo from '../../assets/img/logo.png';
+
+import { render } from 'react-dom';
+import { FlatList } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { preventAutoHide } from 'expo/build/launch/SplashScreen';
 
-export default function Home() {
-  
+
+const data = [
+  { id: '00001', title: 'Book 01' }, 
+  { id: '00002', title: 'Book 02' }, 
+  { id: '00003', title: 'Book 03' },
+  { id: '00004', title: 'Book 04' }, 
+  { id: '00005', title: 'Book 05' }, 
+  { id: '00006', title: 'Book 06' }
+];
+
+const formatData = (data, numColumns) => {
+const numberOfFullRows = Math.floor(data.length / numColumns);
+
+  let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+
+   while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+    data.push({ title: `blank-${numberOfElementsLastRow}`, empty: true });
+     numberOfElementsLastRow = numberOfElementsLastRow + 1;
+  }
+
+  return data;
+};
+
+
+const numColumns = 3;
+
+  export default function Home() {
+  renderItem = ({ item, index }) => {
+      if (item.empty === true) {
+        return
+         <View style={[styles.item, styles.itemInvisable]} />;
+      
+      };
+
   return (
-    <View >
-    <Image
-    style={styles.logo}
-    source={logo}
-  />
-    <TextInput style={styles.box}> 
-    <Text>Pesquisar pelo titulo, autor, categoria </Text>
-    </TextInput>
-    <Text style={styles.txtBox}>
-      Plantando certezas para o futuro
-      Interaja com os livros para descobrir
-      novas aventuras e soltar sua imaginação 
-      </Text>
-      <Text>Indicando para você </Text>
-
-  <View style={styles.wrap}>
-
-      <Text style={styles.book1}>Book 01</Text>
-      <Text style={styles.book2}>Book 02</Text>
-      <Text style={styles.book3}>Book 03</Text>
-      <Text style={styles.book4}>Book 04</Text>
-
+    <View style={styles.container}>
+    <View style={styles.item}>
+      <Text style={styles.itemText}>{item.title}</Text>
     </View>
-    </View>
+</View>
   );
-}
+};
+
+   return (
+    <SafeAreaView style={styles.contain}>
+      <Image style={styles.logo} source={logo}/>
+
+      <TextInput
+      style={styles.SearchBar}>
+        <Text>Pesquisar pelo titulo, autor, categoria 
+        </Text>
+      </TextInput>
+    <View style={styles.box}>
+      <Text>
+        Plantando certezas para o futuro Interaja com 
+        os livros para descobrir novas  aventuras e soltar sua 
+        imaginação
+      </Text>
+    </View>
+      <Text style={{fontSize: 18, left: 5,fontWeight: 'bold', paddingBottom: 5,}}>
+      Indicando para você
+      </Text>
+
+    <FlatList 
+      data={formatData(data, numColumns)}
+      style={styles.container}
+      renderItem={this.renderItem}
+      numColumns={numColumns}
+    />
+    </SafeAreaView>
+    );
+  }
+
+  // All the Style  
 
 const styles = StyleSheet.create({
+  contain:{
+    flex: 1,
+    marginVertical: 10,
+    backgroundColor: '#fff',
+    
+  },
   container:{
     flex: 1,
-    backgroundColor: '#443344',
-    borderBottomColor: '#daaadd',
-    borderBottomWidth: 2,
+    marginVertical: 0,
+    backgroundColor: '#fff'
   },
-  wrap: {
-    margin: 5,
-    top: 30,
+
+  item: {
+    backgroundColor: '#aee',
+    alignItems: 'center',
+    justifyContent:'center',
+    flex: 1,
+    margin: 1,
+    height: Dimensions.get('window').width / numColumns,
+  },
+  itemInvisable: {
+    backgroundColor: 'transparent',
+  },
+  header: {
+    backgroundColor: '#fff',
+    color: '#fff',
+  },
+  logo: {
+  alignItems: 'center',
+  margin: 45,
+
+  },
+  SearchBar: {
+    height: 40, 
+    width: 345,
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    marginTop: 0, 
+    marginBottom: 0, 
+    margin: 35,
+    alignItems:'center',
+    
   },
   box: {
-    backgroundColor: '#fff',
-    color: '#888',
-    height: 50,
-    width: 370,
-    fontSize: 18,
-    borderColor: '#ddd', 
-    borderWidth: 1,
-    textAlign: 'center',
-    margin: 20,
-    borderRadius: 2,
-  },
- txtBox: {
-   width: 370,
-   height:138,
-   fontSize: 14,
-   backgroundColor: '#aaaaaa',
-   borderRadius: 8,
-   padding: 40,
-   margin: 20,
- },
- book1: {
-  backgroundColor: '#8af',
-  color: '#888',
-  height: 220,
-  width: 180,
-  fontSize: 18,
-  borderColor: '#ddd', 
-  borderWidth: 1,
-  textAlign: 'center',
-  borderRadius: 2, 
-  position: "relative",
-  marginLeft: 18,
-  bottom:28,
-  display: 'flex',
-  flexDirection: "row",
-  color: '#000',
- },
- book2: {
-  backgroundColor: '#e5f',
-  color: '#888',
-  height: 220,
-  width: 180,
-  fontSize: 18,
-  borderColor: '#ddd', 
-  borderWidth: 1,
-  textAlign: 'center',
-  borderRadius: 2, 
-  position: "relative",
-  left: 203,
-  bottom:248,
-  display: "flex",
-  flexDirection: "row",
-  color: '#000',
- },
- book3: {
-  backgroundColor: '#fa1',
-  color: '#888',
-  height: 220,
-  width: 180,
-  fontSize: 18,
-  borderColor: '#ddd', 
-  borderWidth: 1,
-  textAlign: 'center',
-  marginTop:125,
-  marginLeft:18,
-  borderRadius: 2, 
-  position: "relative",
-  bottom:367,
-  display: "flex",
-  flexDirection: "row",
-  color: '#000',
- },
- book4: {
-  backgroundColor: '#ff4',
-  color: '#888',
-  height: 220,
-  width: 180,
-  fontSize: 18,
-  borderColor: '#ddd', 
-  borderWidth: 1,
-  textAlign: 'center',
-  borderRadius: 2, 
-  position: "relative",
-  marginLeft:203,
-  bottom:587,
-  display: "flex",
-  flexDirection: "row",
-  color: '#000',
- },
+    backgroundColor: '#aec',
+    color:'#eee333',
+    width: 345,
+    height: 150,
+    margin: 35,
+    padding: 15,
+    borderRadius: 10,
+  }
 });
-
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -21,17 +21,12 @@ export default function Welcome() {
         <Text style={styles.textHero}>Qual seu nome?</Text>
         <TextInput 
         style={styles.TextLabel}
-<<<<<<< HEAD
+
         placeholder= "Digite seu nome ">
 
         </TextInput>
         <TouchableOpacity style={styles.action} onPress={() => navigation.navigate('Home')}>
-=======
-        placeholder="Digite seu nome"
-        
-        ></TextInput>
-        <TouchableOpacity style={styles.action} onPress={() => navigation.navigate('Storys')}>
->>>>>>> 128b7f914fdd5d40889d94dbe02f432923860337
+
           <Text style={styles.actionText}>Entrar</Text>
         </TouchableOpacity> 
     </View>
